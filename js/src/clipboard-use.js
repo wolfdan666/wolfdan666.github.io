a8b1b11dfb2bad7963a28789ee4cff62ca9c9ad2
wolfdan666 <wolfdan666666@gmail.com>
2019-12-10 21:18:00 +0800

    Site updated: 2019-12-10 21:17:30

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 09 2019 19:21:48 GMT+0800 (GMT+08:00)
+// build time:Tue Dec 10 2019 21:17:19 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
