edf5b04e83253412bd5920d9c4ef4baaea1d8d51
wolfdan666 <wolfdan666666@gmail.com>
2020-03-04 12:50:59 +0800

    Site updated: 2020-03-04 12:50:30

---
+++ chlink.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo "请开始你的表演---脚本出自:www.wolfdan.cn  , 作者: 单林敏"
+sum=0
+
+for n in `find . -name "*.md"`
+do
+	((sum++))
+	sed -i "s/https:\/\/raw.githubusercontent.com\/wolfdan666\/BlogPic\/master\//https:\/\/gitee.com\/wolfdan\/BlogPic\/raw\/master\//g" $n
+done
+
+echo "操作了{$sum}个文件"
+echo "请仔细核查,建议使用VScode的git工具查看更改差异更香"
