0eeb9d3370278939665f7357393bd6f075f1334d
wolfdan666 <wolfdan666666@gmail.com>
2020-03-12 14:52:30 +0800

    Site updated: 2020-03-12 14:51:58

--- chlink.sh
+++ chlink.sh
@@ -7,7 +7,8 @@ for n in `find . -name "*.md"`
 do
 	((sum++))
 	#sed -i "s/https:\/\/raw.githubusercontent.com\/wolfdan666\/BlogPic\/master\//https:\/\/gitee.com\/wolfdan\/BlogPic\/raw\/master\//g" $n
-	sed -i "s/https:\/\/gitee.com\/wolfdan\/BlogPic\/raw\/master/https:\/\/www.wolfdan.cn\/img/g" $n
+	#sed -i "s/https:\/\/gitee.com\/wolfdan\/BlogPic\/raw\/master/https:\/\/www.wolfdan.cn\/img/g" $n
+	sed -i "s/https:\/\/www.wolfdan.cn\/img/https:\/\/cdn.jsdelivr.net\/gh\/wolfdan666\/BlogPic/g" $n
 done
 
 echo "操作了{$sum}个文件"
